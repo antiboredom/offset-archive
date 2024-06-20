7d6fb296ec1683ee07253c6f629d6f9e2c80df64
Sam Lavigne <splavigne@gmail.com>
2024-06-20 11:25:36 -0400

    links from about page

--- app/src/routes/about/+page.svelte
+++ app/src/routes/about/+page.svelte
@@ -3,19 +3,23 @@
 
   <p>
     The Offset Archive is a searchable database of every project listed in the
-    four major carbon registories: American Carbon Registry (ACR), Climate
-    Action Reserve (CAR), Gold Standard, and Verra. These platforms make up the
-    majority of the voluntary carbon market and record lists of various carbon
-    offsetting projects, as well as the different methodologies that underlie
-    them. The voluntary market is one of two distinct domains for carbon
-    offsetting today and is open to the public to develop or purchase carbon
-    offsetting products. The other domain is the mandatory or “compliance”
-    marketplaces that form part of regulatory schemes run by governments and
-    which are the basis for reaching international climate commitments. Despite
-    high hopes at the most recent COP meeting in 2023 for regulation of
-    voluntary carbon market, an agreement on key rules for carbon trading was
-    not reached. Voluntary markets remain unregulated by the UN or governmental
-    bodies.
+    four major carbon registories: <a href="https://acrcarbon.org/"
+      >American Carbon Registry (ACR)</a
+    >,
+    <a href="https://www.climateactionreserve.org/"
+      >Climate Action Reserve (CAR)</a
+    >, <a href="https://www.goldstandard.org/">Gold Standard</a>, and
+    <a href="https://verra.org/">Verra</a>. These platforms make up the majority
+    of the voluntary carbon market and record lists of various carbon offsetting
+    projects, as well as the different methodologies that underlie them. The
+    voluntary market is one of two distinct domains for carbon offsetting today
+    and is open to the public to develop or purchase carbon offsetting products.
+    The other domain is the mandatory or “compliance” marketplaces that form
+    part of regulatory schemes run by governments and which are the basis for
+    reaching international climate commitments. Despite high hopes at the most
+    recent COP meeting in 2023 for regulation of voluntary carbon market, an
+    agreement on key rules for carbon trading was not reached. Voluntary markets
+    remain unregulated by the UN or governmental bodies.
   </p>
 
   <p>
@@ -31,7 +35,9 @@
     "retirement" data is from <a
       href="https://gspp.berkeley.edu/research-and-impact/centers/cepp/projects/berkeley-carbon-trading-project"
       >The Berkeley Carbon Trading Project</a
-    >. The Offset Archive is part of a collaborative artistic research project
+    >. Code for this project can be found on
+    <a href="https://github.com/antiboredom/offset-archive">GitHub</a>. The
+    Offset Archive is part of a collaborative artistic research project
     <em>Offset</em>, by <a href="https://lav.io">Sam Lavigne</a> and
     <a href="https://tegabrain.com">Tega Brain</a>.
   </p>
